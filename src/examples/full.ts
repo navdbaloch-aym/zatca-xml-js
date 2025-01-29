9038100a60649de166e55f706fe7a1d99bdee852
navdbaloch-aym <102135331+navdbaloch-aym@users.noreply.github.com>
2025-01-29 11:13:42 +0000

    Fixed warnings

--- src/examples/full.ts
+++ src/examples/full.ts
@@ -19,6 +19,36 @@ const line_item: ZATCASimplifiedInvoiceLineItem = {
     ]
 };
 
+function fixPrecision(value: number, precision?: number, roundOff = true): number {
+    if (isNaN(value)) return 0;
+
+    const parsedValue = Number(value);
+    if (!precision) return parsedValue;
+
+    if (roundOff) return parseFloat(parsedValue.toFixed(precision));
+    else return parseFloat(toFixedNoRounding(parsedValue, precision));
+}
+
+function toFixedNoRounding(n: number, fractionDigits: number): string {
+    const reg = new RegExp('^-?\\d+(?:\\.\\d{0,' + fractionDigits + '})?', 'g');
+    const m = n.toString().match(reg);
+
+    if (m?.length) {
+        const a: string = m[0];
+        const dot: number = a.indexOf('.');
+        if (dot === -1) return a + '.' + '0'.repeat(fractionDigits);
+
+        const b: number = fractionDigits - (a.length - dot) + 1;
+        return b > 0 ? a + '0'.repeat(b) : a;
+    }
+
+    return '0.00';
+}
+
+function calculateTaxExcPrice(taxIncPrice: number) {
+    return fixPrecision(taxIncPrice / (1 + 15 / 100), 2, false);
+}
+
 // Sample EGSUnit
 const egsunit: EGSUnitInfo = {
     uuid: "6f4d20e0-6bfe-4a80-9389-7dabe6620f12",
@@ -38,64 +68,64 @@ const egsunit: EGSUnitInfo = {
     branch_name: "My Branch Name",
     branch_industry: "Food"
 };
+const lineItems = [
+    {
+        id: "1",
+        name: "item1",
+        quantity: 10,
+        price: 25,
+        tax: 3.260869565,
+        total_price: 250.0,
+        total_tax: 32.60869565,
+        VAT_percent: 0.15,
+        other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
+        discounts: []
+    },
+    {
+        id: "2",
+        name: "item2",
+        quantity: 10,
+        price: 25,
+        tax: 3.260869565,
+        total_price: 250.0,
+        total_tax: 32.60869565,
+        VAT_percent: 0.15,
+        other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
+        discounts: [
+        ]
+    },
+    {
+        id: "1",
+        name: "Delivery charges",
+        quantity: 1,
+        price: 20,
+        tax: 20 - calculateTaxExcPrice(20),
+        total_price: 20,
+        total_tax: (20 - calculateTaxExcPrice(20)),
 
+        VAT_percent: 0.15,
+        other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
+        discounts: []
+    }
+];
 // Sample Invoice
 const invoice = new ZATCASimplifiedTaxInvoice({
     props: {
         egs_info: egsunit,
-        allowance_total: 10,
-        total_exl_amount: 500 - 65.2,
-        total_tax: 65.2,
-        total_inclusive_amount: 500,
+        // allowance_total: 100,
+        total_exl_amount: 520-67.8268696,
+        total_tax: 67.82608696,
+        total_inclusive_amount: 520,
         invoice_counter_number: 1,
         invoice_serial_number: "EGS1-886431145-1",
         issue_date: "2022-03-13",
         issue_time: "14:40:40",
         buyer_name: "Naveed Baloch",
         previous_invoice_hash: "NWZlY2ViNjZmZmM4NmYzOGQ5NTI3ODZjNmQ2OTZjNzljMmRiYzIzOWRkNGU5MWI0NjcyOWQ3M2EyN2ZiNTdlOQ==",
-        line_items: [
-            {
-                id: "1",
-                name: "item1",
-                quantity: 10,
-                price: 25,
-                tax: 3.26,
-                total_price: 25 * 10,
-                total_tax: 3.26 * 10,
-                VAT_percent: 0.15,
-                other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
-                discounts: []
-            },
-            {
-                id: "2",
-                name: "item2",
-                quantity: 10,
-                price: 25,
-                tax: 3.26,
-                total_price: 25 * 10,
-                total_tax: 3.26 * 10,
-                VAT_percent: 0.15,
-                other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
-                discounts: [
-                ]
-            },
-            {
-                id: "1",
-                name: "Delivery charges",
-                quantity: 1,
-                price: 20,
-                tax: (20 * 0.15),
-                total_price: 20,
-                total_tax: (20 * 0.15),
-                VAT_percent: 0.15,
-                other_taxes: [], // emptied other taxes to comply with Zatca BR-KSA-84
-                discounts: [
-                ]
-            }
-        ]
+        line_items: lineItems
     }
 });
-
+console.log(invoice)
 
 const main = async () => {
     try {
@@ -125,6 +155,7 @@ const main = async () => {
         //  // Report invoice production
         //  // Note: This request currently fails because ZATCA sandbox returns a constant fake production certificate
         let reportedInvoice = await egs.reportInvoice(signed_invoice_string, invoice_hash);
+        console.log(JSON.stringify(reportedInvoice?.validationResults, null, 2));
         console.log("Reporting Status: ", reportedInvoice?.reportingStatus);
     } catch (error: any) {
         console.log(JSON.stringify(error?.response?.data || { message: error.message }, null, 2));
--- src/zatca/ZATCASimplifiedTaxInvoice.ts
+++ src/zatca/ZATCASimplifiedTaxInvoice.ts
@@ -111,11 +111,11 @@ export class ZATCASimplifiedTaxInvoice {
         cacTaxTotal = {
             "cbc:TaxAmount": {
                 "@_currencyID": "SAR",
-                "#text": line_item_total_taxes.toFixedNoRounding(2)
+                "#text": line_item_total_taxes.toFixed(2)
             },
             "cbc:RoundingAmount": {
                 "@_currencyID": "SAR",
-                "#text": line_item.total_price.toFixedNoRounding(2)
+                "#text": line_item.total_price.toFixed(2)
             }
         };
 
@@ -161,7 +161,7 @@ export class ZATCASimplifiedTaxInvoice {
                 "cac:Price": {
                     "cbc:PriceAmount": {
                         "@_currencyID": "SAR",
-                        "#text": (line_item.price - line_item.tax).toFixedNoRounding(2)
+                        "#text": (line_item.price - line_item.tax).toFixed(2)
                     },
 
                 }
@@ -176,7 +176,7 @@ export class ZATCASimplifiedTaxInvoice {
 
 
     private constructLegalMonetaryTotal = (tax_inclusive_subtotal: number, taxes_total: number, allowances_total: number) => {
-        const tax_exl_amount = (tax_inclusive_subtotal - taxes_total).toFixedNoRounding(2);
+        const tax_exl_amount = (tax_inclusive_subtotal - taxes_total).toFixed(2);
         return {
             // BR-DEC-09
             "cbc:LineExtensionAmount": {
@@ -191,7 +191,7 @@ export class ZATCASimplifiedTaxInvoice {
             // BR-DEC-14, BT-112
             "cbc:TaxInclusiveAmount": {
                 "@_currencyID": "SAR",
-                "#text": tax_inclusive_subtotal.toFixedNoRounding(2)
+                "#text": tax_inclusive_subtotal.toFixed(2)
             },
             "cbc:AllowanceTotalAmount": {
                 "@_currencyID": "SAR",
@@ -204,7 +204,7 @@ export class ZATCASimplifiedTaxInvoice {
             // BR-DEC-18, BT-112
             "cbc:PayableAmount": {
                 "@_currencyID": "SAR",
-                "#text": (parseFloat((tax_inclusive_subtotal + taxes_total).toFixed(2))).toFixedNoRounding(2)
+                "#text": tax_inclusive_subtotal.toFixedNoRounding(2)
             }
         }
     }
@@ -213,16 +213,16 @@ export class ZATCASimplifiedTaxInvoice {
 
         const cacTaxSubtotal: any[] = [];
         // BR-DEC-13, MESSAGE : [BR-DEC-13]-The allowed maximum number of decimals for the Invoice total VAT amount (BT-110) is 2.
-        const addTaxSubtotal = (taxable_amount: number, tax_amount: number, tax_percent: number) => {
+        const addTaxSubtotal = (tax_incl_amt: number, tax_amount: number, tax_percent: number) => {
             cacTaxSubtotal.push({
                 // BR-DEC-19
                 "cbc:TaxableAmount": {
                     "@_currencyID": "SAR",
-                    "#text": taxable_amount.toFixedNoRounding(2)
+                    "#text": (tax_incl_amt-tax_amount).toFixedNoRounding(2)
                 },
                 "cbc:TaxAmount": {
                     "@_currencyID": "SAR",
-                    "#text": tax_amount.toFixedNoRounding(2)
+                    "#text": tax_amount.toFixed(2)
                 },
                 "cac:TaxCategory": {
                     "cbc:ID": {
@@ -256,8 +256,6 @@ export class ZATCASimplifiedTaxInvoice {
         const tax_percentage = line_items.map(li => li.VAT_percent).sort((a, b) => b - a)[0];
         addTaxSubtotal(props.total_inclusive_amount, taxes_total, tax_percentage);
 
-        // BT-110
-        taxes_total = parseFloat(taxes_total.toFixedNoRounding(2));
 
         // BR-DEC-13, MESSAGE : [BR-DEC-13]-The allowed maximum number of decimals for the Invoice total VAT amount (BT-110) is 2.
         return [
@@ -265,7 +263,7 @@ export class ZATCASimplifiedTaxInvoice {
                 // Total tax amount for the full invoice
                 "cbc:TaxAmount": {
                     "@_currencyID": "SAR",
-                    "#text": taxes_total.toFixedNoRounding(2)
+                    "#text": taxes_total.toFixed(2)
                 },
                 "cac:TaxSubtotal": cacTaxSubtotal,
             },
@@ -273,7 +271,7 @@ export class ZATCASimplifiedTaxInvoice {
                 // KSA Rule for VAT tax
                 "cbc:TaxAmount": {
                     "@_currencyID": "SAR",
-                    "#text": taxes_total.toFixedNoRounding(2)
+                    "#text": taxes_total.toFixed(2)
                 }
             }
         ];
@@ -294,7 +292,6 @@ export class ZATCASimplifiedTaxInvoice {
         });
 
         // BT-110
-        total_taxes = parseFloat(total_taxes.toFixedNoRounding(2))
         total_inclusive_amount = parseFloat(total_inclusive_amount.toFixedNoRounding(2))
         line_total_allowances = parseFloat(line_total_allowances.toFixedNoRounding(2))
 
